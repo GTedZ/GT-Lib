fbbea7ec351b8a8643ca512a50709217c2ec90cb
GTedZ <Hellis_over@icloud.com>
2022-09-03 07:44:49 +0300

    added getDifference

---
+++ JSLib.js
@@ -0,0 +1,9 @@
+module.exports = () => {
+
+
+    return {
+        getDifference: function (val1, val2, precision = false) {
+            return precision ? Math.floor((val1 - val2) * Math.pow(10, precision)) / Math.pow(10, precision) : (val1 - val2) / val2;
+        }
+    }
+}
